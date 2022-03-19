28efd3cdf9f6dcb3463776ebd8eccf8c873df825
twilightscapes <ihatespam@gmail.com>
2022-03-19 01:06:48 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -106,17 +106,17 @@ export const pageQuery = graphql`
   
     capability1: file(relativePath: { eq: "measurement.jpg" }) {
       childImageSharp {
-        gatsbyImageData(placeholder: BLURRED, quality:40, width:800, layout: CONSTRAINED)
+        gatsbyImageData(placeholder: BLURRED, quality:40, layout: CONSTRAINED,)
       }
     }
     capability2: file(relativePath: { eq: "front-building.jpg" }) {
       childImageSharp {
-        gatsbyImageData(placeholder: BLURRED, quality:40, width:800, layout: CONSTRAINED)
+        gatsbyImageData(placeholder: BLURRED, quality:40, layout: CONSTRAINED)
       }
     }
     capability3: file(relativePath: { eq: "facilities.jpg" }) {
       childImageSharp {
-        gatsbyImageData(placeholder: BLURRED, quality:40, width:800, layout: CONSTRAINED)
+        gatsbyImageData(placeholder: BLURRED, quality:40, layout: CONSTRAINED)
       }
     }
 
